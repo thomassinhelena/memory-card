e808b715cbba32ef49db9366811bb6ab6974d898
Hélèna Thomassin <thomassin.helena@gmail.com>
2020-01-07 22:46:58 +0100

    add board, cards and images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import Cards from './Components/Cards';
-import './App.css';
+import React, {useState} from 'react'
+import Board from './Components/Board';
+
 
 
 function App() {
+  const cards = buildCards()
   return (
     <div className="App">
-     <Cards/>  
+      <Board cards={cards} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+function buildCards() {
+  let id = 0
+  const images = [
+    process.env.PUBLIC_URL + '/img/agnodice.png', 
+    process.env.PUBLIC_URL + '/img/AnnetteKellerman.png',
+    process.env.PUBLIC_URL + '/img/NellyBly.png',
+    process.env.PUBLIC_URL + '/img/wuzetian.png'
+  ]
+
+  const backImg = process.env.PUBLIC_URL + '/img/backImg.png'
+  const cards = Object.keys(images).reduce((result, item) => {
+    const getCard = () => ({
+      id: id++,
+      type: item,
+      backImg,
+      frontImg: images[item],
+      flipped: false,
+    })
+    return [...result, getCard(), getCard()]
+  }, [])
+  return suffle(cards)
+}
+
+function suffle(arr) {
+  let len = arr.length
+  for (let i = 0; i < len; i++) {
+    let randomIdx = Math.floor(Math.random() * len)
+    let copyCurrent = {...arr[i]}
+    let copyRandom = {...arr[randomIdx]}
+    arr[i] = copyRandom
+    arr[randomIdx] = copyCurrent
+  }
+  return arr
+}
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,56 +1,20 @@
 import React from 'react';
 import './Cards.css';
 
+const Card = props => {
+  const {frontImg, backImg, flipped, onClick} = props
+  const img = flipped ? frontImg : backImg
+  return (
+    <div className="Card" onClick={onClick}>
+      <img src={process.env.PUBLIC_URL + '/img/agnodice.png'} />
+      <img src={process.env.PUBLIC_URL + '/img/AnnetteKellerman.png'} />
+      <img src={process.env.PUBLIC_URL + '/img/NellyBly.png'} />
+      <img src={process.env.PUBLIC_URL + '/img/wuzetian.png'} />
+      <img src={process.env.PUBLIC_URL + '/img/backImg.png'} />
+      <img src={img} alt=""/>
+    </div>
+  )
+};
 
-
-function Cards() {
-  console.log(process);
-  console.log(process.env);
-  console.log(process.env.PUBLIC_URL);
-return(
-      
-  <div>
-    <section>
-      <div className="card">
-        <img className="card-img-top" src={process.env.PUBLIC_URL+'/img/agnodice.png'} />
-        <div className="card-body">
-          <h5 className="card-title">Marine</h5>
-        </div>
-      </div>
-    </section>
-    
-
-    <section>
-      <div className="card">
-        <img className="card-img-top" src={process.env.PUBLIC_URL+'/img/AnnetteKellerman.png'} />
-        <div className="card-body">
-          <h5 className="card-title">Camille</h5>
-        </div>
-      </div>
-    </section>
-
-    <section>
-      <div className="card">
-        <img className="card-img-top" src={process.env.PUBLIC_URL+'/img/NellyBly.png'} />
-        <div className="card-body">
-          <h5 className="card-title">Peggy</h5>
-        </div>
-      </div>
-    </section>
-
-    <section>
-      <div className="card">
-        <img className="card-img-top" src={process.env.PUBLIC_URL+'/img/wuzetian.png'} />
-        <div className="card-body">
-          <h5 className="card-title">Sue</h5>
-        </div>
-      </div>
-    </section>
-  </div> 
-)};
-
-export default Cards;
-
-
-
+export default Card;
       
