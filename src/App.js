22aaa58b8b7976e5f8af5ce39c0f038366d10662
Hélèna Thomassin <thomassin.helena@gmail.com>
2020-03-10 21:21:04 +0100

    fixed build

--- src/App.js
+++ src/App.js
@@ -1,50 +1,97 @@
-import React, {useState} from 'react'
-import Board from './Components/Board';
+import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
 
+import './App.css'
+import Cards from './Components/Cards'
+import GuessCount from './Components/GuessCount'
+import HallOfFame, { FAKE_HOF } from './Components/HallOfFame'
 
+const SIDE = 6
+const SYMBOLS = '🎃🎂🎅🐰🎥🍂👨💪🎓👩🎊🏊👑☪🌱☘☀🏈💘⚽'
+const VISUAL_PAUSE_MSECS = 750
 
-function App() {
-  const cards = buildCards()
-  return (
-    <div className="App">
-      <Board cards={cards} />
-    </div>
-  )
+class App extends Component {
+  state = {
+    cards: this.generateCards(),
+    currentPair: [],
+    guesses: 0,
+    matchedCardIndices: []
+  }
+
+  getFeedbackForCard(index) {
+    const { currentPair, matchedCardIndices } = this.state
+    const indexMatched = matchedCardIndices.includes(index)
+
+    if (currentPair.length < 2) {
+      return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
+    }
+
+    if (currentPair.includes(index)) {
+      return indexMatched ? 'justMatched' : 'justMismatched'
+    }
+
+    return indexMatched ? 'visible' : 'hidden'
+  }
+  
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
+  }
+
+  // Arrow fx for binding
+  handleCardClick = index => {
+    const { currentPair } = this.state
+
+    if (currentPair.length === 2) {
+      return
+    }
+
+    if (currentPair.length === 0) {
+      this.setState({ currentPair: [index] })
+      return
+    }
+
+    this.handleNewPairClosedBy(index)
+  }
+
+  handleNewPairClosedBy(index) {
+    const { cards, currentPair, guesses, matchedCardIndices } = this.state
+
+    const newPair = [currentPair[0], index]
+    const newGuesses = guesses + 1
+    const matched = cards[newPair[0]] === cards[newPair[1]]
+    this.setState({ currentPair: newPair, guesses: newGuesses })
+    if (matched) {
+      this.setState({ matchedCardIndices: [...matchedCardIndices, ...newPair] })
+    }
+    setTimeout(() => this.setState({ currentPair: [] }), VISUAL_PAUSE_MSECS)
+  }
+
+  render() {
+    const { cards, guesses, matchedCardIndices } = this.state
+    const won = matchedCardIndices.length === cards.length
+    return (
+      <div className="memory">
+        <GuessCount guesses={guesses} />
+        {cards.map((card, index) => (
+          <Cards
+            card={card}
+            feedback={this.getFeedbackForCard(index)}
+            key={index}
+            index={index}
+            onClick={this.handleCardClick}
+          />
+        ))}
+        {won && <HallOfFame entries={FAKE_HOF} />}
+      </div>
+    )
+  }
 }
 
 export default App
-
-function buildCards() {
-  let id = 0
-  const images = [
-    process.env.PUBLIC_URL + '/img/agnodice.png', 
-    process.env.PUBLIC_URL + '/img/AnnetteKellerman.png',
-    process.env.PUBLIC_URL + '/img/NellyBly.png',
-    process.env.PUBLIC_URL + '/img/wuzetian.png'
-  ]
-
-  const backImg = process.env.PUBLIC_URL + '/img/backImg.png'
-  const cards = Object.keys(images).reduce((result, item) => {
-    const getCard = () => ({
-      id: id++,
-      type: item,
-      backImg,
-      frontImg: images[item],
-      flipped: false,
-    })
-    return [...result, getCard(), getCard()]
-  }, [])
-  return suffle(cards)
-}
-
-function suffle(arr) {
-  let len = arr.length
-  for (let i = 0; i < len; i++) {
-    let randomIdx = Math.floor(Math.random() * len)
-    let copyCurrent = {...arr[i]}
-    let copyRandom = {...arr[randomIdx]}
-    arr[i] = copyRandom
-    arr[randomIdx] = copyCurrent
-  }
-  return arr
-}
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import './Cards.css';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-const Card = props => {
-  const {frontImg, backImg, flipped, onClick} = props
-  const img = flipped ? frontImg : backImg
-  return (
-    <div className="Card" onClick={onClick}>
-      <img src={process.env.PUBLIC_URL + '/img/agnodice.png'} />
-      <img src={process.env.PUBLIC_URL + '/img/AnnetteKellerman.png'} />
-      <img src={process.env.PUBLIC_URL + '/img/NellyBly.png'} />
-      <img src={process.env.PUBLIC_URL + '/img/wuzetian.png'} />
-      <img src={process.env.PUBLIC_URL + '/img/backImg.png'} />
-      <img src={img} alt=""/>
+import './Cards.css'
+
+const HIDDEN_SYMBOL = '❓'
+
+const Card = ({ card, feedback, index, onClick }) => (
+    <div className={`card ${feedback}`} onClick={() => onClick(index)}>
+        <span className="symbol">
+            {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
+        </span>
     </div>
-  )
-};
+)
+
+Card.propTypes = {
+    card: PropTypes.string.isRequired,
+    feedback: PropTypes.oneOf([
+        'hidden',
+        'justMatched',
+        'justMismatched',
+        'visible',
+    ]).isRequired,
+    index: PropTypes.number.isRequired,
+    onClick: PropTypes.func.isRequired,
+}
 
 export default Card;
-      
